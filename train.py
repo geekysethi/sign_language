4de946aa9a080e8f2d62f6c956cc38286b9e4431
geekysethi <sethiaashish1995@gmail.com>
2017-11-05 23:09:29 +0530

    after cleaning

--- train.py
+++ train.py
@@ -7,9 +7,10 @@ import tqdm
 from random import shuffle
 import pickle
 from os.path import basename
-path="/home/geekysethi/Desktop/vrinda_project/Dataset/*"
-path_image="/home/geekysethi/Desktop/vrinda_project/Dataset"
-path_dir=np.sort(glob.glob(path))
+
+path="/home/geekysethi/Desktop/vrinda_project/Dataset"
+# path_image="/home/geekysethi/Desktop/vrinda_project/Dataset"
+path_dir=np.sort(glob.glob(path+'/*'))
 
 
 # print(path_dir)
@@ -28,17 +29,17 @@ print(demo_path)
 
 df= pd.read_csv(demo_path)       
 print(df.head())
-print(os.path.join(path_image,df.image[0]))
-print(basename(os.path.join(path_image,df.image[0]))[0])
+print(os.path.join(path,df.image[0]))
+print(basename(os.path.join(path,df.image[0]))[0])
 
 
 for i in range(len(df)):
 
 	print(i)
-	image_path=os.path.join(path_image,df.image[i])
+	image_path=os.path.join(path,df.image[i])
 	img=cv2.imread(image_path,1)
 	img=img[df.top_left_y[i]:df.bottom_right_y[i],df.top_left_x[i]:df.bottom_right_x[i]]
-	label=basename(os.path.join(path_image,df.image[0]))[0]
+	label=basename(os.path.join(path,df.image[0]))[0]
 
 	train_images.append(img)
 	labels.append(label)
